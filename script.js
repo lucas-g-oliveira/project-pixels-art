c9f7a5d1c1a1a7c80624d05e972219205b9e370a
Lucas G <lucasg.devmail93@gmail.com>
2022-07-04 18:30:07 -0300

    requisito 3 OK

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-let  classColorsPixels = 'color';
+//classe pixel
+
+let colorsPalet = {
+    'black': 'black',
+    'red': 'red',
+    'green': 'green',
+    'blue': 'blue',
+    'yellow': 'yellow',
+    'orange': 'orange',
+}
+
+//cria a paleta
+for (data in colorsPalet) {
+    let elementSRC = document.querySelector('#color-palette');
+
+    let createTempElement = document.createElement('div');
+    createTempElement.classList = 'color';
+    createTempElement.id = data;
+    createTempElement.style.backgroundColor = colorsPalet[data];
+
+    elementSRC.appendChild(createTempElement);
+}
+
+//cria a grade inicial, com 25 px
+for (let index = 1; index <= 25; index += 1) {
+    let elementSRC = document.querySelector('#pixel-board');
+
+    let createTempElement = document.createElement('div');
+    createTempElement.classList = 'pixel';
+    createTempElement.id = 'id-pixel-'+index;
+    createTempElement.style.backgroundColor = 'rgb(255,255,255)';
+
+    elementSRC.appendChild(createTempElement);
+}
